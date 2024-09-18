b70db3f70047949eb296ef2ee1d4b11d55cbf48c
datexp-723 <michael.aring@live.com>
2024-09-18 15:01:01 +0200

    Update icons + bgc

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#043',
+    backgroundColor: '#006EFF',
     alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'row',
