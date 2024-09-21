9a8c9e0fa1d7ccde63ad413a91c6777e224951de
datexp-723 <michael.aring@live.com>
2024-09-21 09:33:26 +0200

    dark theme

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Appearance, useColorScheme, StyleSheet, Text, View } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
+  const themeContainerStyle =
+    colorScheme === 'light' ? 
+      styles.lightContainer : 
+      styles.darkContainer;
 
   const [orientationIsLandscape,setOrientation]=useState(true)
   const [currentCount, setCurrentCount] = useState(0);
@@ -18,33 +25,40 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>{currentCount}</Text>
+    // <SafeAreaView style={{ flex: 1 }}>
+      <View style={[styles.container, themeContainerStyle]}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>{currentCount}</Text>
+        </View>
+        <View style={styles.counter}>
+              <IconButton icon="minus-thick" 
+                          iconColor="white"
+                          size={100}
+                          style={styles.image}
+                          onPress={() => onDecrementCounter()} />
+              <IconButton icon="plus-thick"
+                          iconColor="white"         
+                          size={100}
+                          style={styles.image}
+                          onPress={() => onIncrementCounter()} />
+          <StatusBar style="auto" />
+        </View>
       </View>
-      <View style={styles.counter}>
-            <IconButton icon="minus-thick" 
-                        iconColor="white"
-                        size={100}
-                        style={styles.image}
-                        onPress={() => onDecrementCounter()} />
-            <IconButton icon="plus-thick"
-                        iconColor="white"         
-                        size={100}
-                        style={styles.image}
-                        onPress={() => onIncrementCounter()} />
-        <StatusBar style="auto" />
-      </View>
-    </View>
+    // </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#006EFF',
     alignItems: 'center',
     flexDirection: 'column',
+  },
+  lightContainer: {
+    backgroundColor: '#006EFF',
+  },
+  darkContainer: {
+    backgroundColor: '#000000',
   },
   image: {
     marginLeft: 25,
